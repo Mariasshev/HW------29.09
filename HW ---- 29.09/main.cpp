bf8396f54f2fcc092eb6cb4c1c4f3f6fae39f4d0
Maria Shevchenko <sevcenkomaria72@gmail.com>
2023-09-29 15:35:23 +0300

    Добавьте файлы проекта.

---
+++ HW ---- 29.09/main.cpp
@@ -0,0 +1,50 @@
+﻿#pragma warning(disable: 4996)
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    //my path for check: C:\Users\Admin\source\repos\HW ---- 29.09\HW ---- 29.09\index.html
+    char path[256];
+    cout << "Enter path: ";
+    cin.getline(path, sizeof(path));
+    ifstream in(path);
+
+    char string;
+    int errorCounter = 0;
+    int count_open=0;
+    int count_close=0;
+    if (in.is_open())
+    {
+        while (!in.eof())
+        {
+            in.get(string);
+            cout << string << ' ';
+            if (string == '<') {
+                count_open++;
+            }
+            else if (string == '>') {
+                count_close++;
+            }
+        }
+        if (count_close > count_open) {
+            int rez = count_close - count_open;
+            cout << "Find " << rez << "errors!" << endl;
+        }
+        else if (count_close < count_open) {
+            int rez = count_open - count_close;
+            cout << "Find " << rez << " errors!" << endl;
+        }
+        else{
+            cout << "All is correct!" << endl;
+        }
+        in.close();
+    }
+    else
+    {
+        cout << "Could not open the file" << endl;
+    }
+}
